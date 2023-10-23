51c64bfc629c853c7ebb009c1686fa8af0c66fab
Pedro Antônio Silva <105285216+P-ASilva@users.noreply.github.com>
2023-10-23 15:10:30 -0300

    first route and small fixes

--- vendas/src/main/java/com/api/vendas/Vendas/VendasController.java
+++ vendas/src/main/java/com/api/vendas/Vendas/VendasController.java
@@ -2,8 +2,19 @@ package com.api.vendas.Vendas;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @RestController
-@RequestMapping("/venda")
+@RequestMapping("/vendas")
 public class VendasController {
-    
+    @Autowired
+    private VendasService vendasService;
+
+    public List<Venda> getVendas() { // Is DTO required?
+        return vendasService.getVendas();
+    }
+
 }
--- vendas/src/main/java/com/api/vendas/Vendas/VendasRepository.java
+++ vendas/src/main/java/com/api/vendas/Vendas/VendasRepository.java
@@ -3,6 +3,6 @@ package com.api.vendas.Vendas;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface VendasRepository MongoRepository<Venda, String> {
+public interface VendasRepository extends MongoRepository<Venda, String> {
     
 }
