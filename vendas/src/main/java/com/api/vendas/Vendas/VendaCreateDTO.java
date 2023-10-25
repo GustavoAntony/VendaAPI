849030cc6c0d9505c0431519e6f14694afd7f9ca
C0D8 <matheusaguiar362@gmail.com>
2023-10-25 15:40:08 -0300

    Erik e Matheus Botando quente no mockoon, manda mais que tá fraco

---
+++ vendas/src/main/java/com/api/vendas/Vendas/VendaCreateDTO.java
@@ -0,0 +1,14 @@
+package com.api.vendas.Vendas;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VendaCreateDTO {
+
+    private String cpfCliente;
+    private String cpfCorretor;
+    private String idenifierImovel;
+
+}
--- vendas/src/main/java/com/api/vendas/Vendas/VendasService.java
+++ vendas/src/main/java/com/api/vendas/Vendas/VendasService.java
@@ -17,19 +17,21 @@ public class VendasService {
         return vendasRepository.findAll();
     }
 
-    public List<Venda> getVendasByPropertyIdentifier(String pid) {
-        return vendasRepository.findByPropertyIdentifier(pid);
-    }
-
-    public List<Venda> getVendasByLocatorCpf(String locatorCpf) {
-        return vendasRepository.findByLocatorCpf(locatorCpf);
-    }
-    public List<Venda> getVendasByUserCpf(String userCpf) {
-        return vendasRepository.findByLocatorCpf(userCpf);
-    }
 
 
-    public Venda postVenda(String imovelIdentifier, String corretorIdentifier, String clienteIdentifier){
+    public List<Venda> getVendasByCpfCorretor(String corretorCpf) {
+        return vendasRepository.findByCpfCorretor(corretorCpf);
+    }
+    public List<Venda> getVendasByCpfCliente(String clienteCpf) {
+        return vendasRepository.findByCpfCliente(clienteCpf);
+    }
+
+    public List<Venda> getVendasByStatus(String status) {
+        return vendasRepository.findByVendaStatus(status);
+    }
+
+
+    public Venda postVenda(VendaCreateDTO vendaCreateDTO){
 
         Venda venda = new Venda();
 
@@ -38,7 +40,7 @@ public class VendasService {
         venda.setVendaStatus("SUCESSO");
 
         ResponseEntity<ImoveisDTO> responseImoveis =
-                restTemplate.getForEntity("http://localhost:8080/imevoeis/" + imovelIdentifier, ImoveisDTO.class);
+                restTemplate.getForEntity("http://localhost:8081/imoveis/" + vendaCreateDTO.getIdenifierImovel(), ImoveisDTO.class);
         if (!responseImoveis.getStatusCode().is2xxSuccessful()) {
             venda.setVendaStatus("ERRO");
             throw new ImovelNotFoundException();
@@ -47,19 +49,19 @@ public class VendasService {
         venda.setImovelIdentifier(responseImoveis.getBody().getIdentifier());
 
         ResponseEntity<CorretorDTO> responseCorretor =
-                restTemplate.getForEntity("http://localhost:8080/corretor/" + corretorIdentifier, CorretorDTO.class);
+                restTemplate.getForEntity("http://localhost:8081/corretor/" + vendaCreateDTO.getCpfCorretor(), CorretorDTO.class);
         if (!responseCorretor.getStatusCode().is2xxSuccessful()) {
             venda.setVendaStatus("ERRO");
         }
 
-        venda.setCpfCorretor(responseCorretor.getCpfCorretor());
+        venda.setCpfCorretor(responseCorretor.getBody().getCpf());
 
         ResponseEntity<ClienteDTO> responseCliente =
-                restTemplate.getForEntity("http://localhost:8080/cliente/" + clienteIdentifier, ClienteDTO.class);
+                restTemplate.getForEntity("http://localhost:8081/cliente/" + vendaCreateDTO.getCpfCliente(), ClienteDTO.class);
         if(! responseCliente.getStatusCode().is2xxSuccessful()){
             venda.setVendaStatus("ERRO");
         }
-        venda.setCpfCliente(responseCliente.getCpfCliente());
+        venda.setCpfCliente(responseCliente.getBody().getCpf());
 
 
         return vendasRepository.save(venda);
