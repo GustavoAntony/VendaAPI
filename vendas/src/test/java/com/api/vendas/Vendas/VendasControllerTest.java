e16b612d8c82e3b21d9e124063dd01d2abbdaa5f
Pedro Antônio Silva <105285216+P-ASilva@users.noreply.github.com>
2023-10-31 15:10:32 -0300

    unfinished post test

--- vendas/src/test/java/com/api/vendas/Vendas/VendasControllerTest.java
+++ vendas/src/test/java/com/api/vendas/Vendas/VendasControllerTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -18,6 +20,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class VendasControllerTest {
@@ -105,4 +109,58 @@ public class VendasControllerTest {
         Assertions.assertEquals(om.writeValueAsString(vendas), resp);
     }
 
+    @Test
+    void test_listVendasByClienteCpf_UmaVenda() throws Exception {
+
+        Venda venda = new Venda();
+        venda.setCpfCliente("1234");
+
+        List<Venda> vendas = new ArrayList<>();
+        vendas.add(venda);
+
+        Mockito.when(vendasService.getVendasByCpfCliente("1234")).thenReturn(vendas);
+
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.get("/vendas/cliente").param("clienteCpf", "1234"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        ObjectMapper om = new ObjectMapper();
+
+        String resp = result.getResponse().getContentAsString();
+        Assertions.assertEquals(om.writeValueAsString(vendas), resp);
+    }
+    @Test
+    void test_criaVendaValida() throws Exception {
+        VendaCreateDTO venda = new VendaCreateDTO(); // something wrong here
+        venda.setCpfCliente("1245");
+        venda.setCpfCorretor("123");
+        venda.setIdenifierImovel("3");
+
+        Venda venda2 = new Venda();
+        venda2.setCpfCliente("1245");
+        venda2.setCpfCorretor("123");
+        venda2.setImovelIdentifier("3");
+        venda2.setVendaStatus("SUCESSO");
+
+
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper.writeValueAsString(venda);
+
+        Mockito.when(vendasService.postVenda(ArgumentMatchers.eq(venda))).thenReturn(venda2);
+
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.post("/vendas")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                        //.characterEncoding("utf-8"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        ObjectMapper om = new ObjectMapper();
+        String resp = result.getResponse().getContentAsString();
+
+        Assertions.assertEquals(om.writeValueAsString(venda2), resp);
+    }
 }
