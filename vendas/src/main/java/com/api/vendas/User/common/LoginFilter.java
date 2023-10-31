99fb316f919ff3d3aedf0fdcca5e70e4a5f4a07b
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2023-10-31 12:38:22 -0300

    adicionei erro no terminal

--- vendas/src/main/java/com/api/vendas/User/common/LoginFilter.java
+++ vendas/src/main/java/com/api/vendas/User/common/LoginFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import com.api.vendas.User.user.exception.TokenNotPassedException;
 
 import jakarta.servlet.*;
 import org.springframework.web.client.RestTemplate;
@@ -20,14 +21,18 @@ public class LoginFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
         HttpServletRequest req = (HttpServletRequest) request;
-
         String token = req.getHeader("token");
 
         RestTemplate restTemplate = new RestTemplate();
 
-        ResponseEntity<ReturnUserDTO> res = restTemplate.getForEntity("http://54.71.150.144:8082/token/" + token, ReturnUserDTO.class);
+        ResponseEntity<ReturnUserDTO> res = null;
+        try {
+            res = restTemplate.getForEntity("http://54.71.150.144:8082/token/" + token, ReturnUserDTO.class);
+        } catch (Exception e) {
+            throw new UserNotFoundException();
+        }
 
-        if (res.getStatusCode().is2xxSuccessful()){
+        if (res.getStatusCode().is2xxSuccessful()) {
             chain.doFilter(request, response);
         } else {
             throw new UserNotFoundException();
