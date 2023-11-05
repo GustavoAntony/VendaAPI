41dab55ee4c26ca3a6d1056e00c39ddc45b6f902
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2023-11-05 11:22:29 -0300

    ajustando o post venda

--- vendas/src/main/java/com/api/vendas/Vendas/VendasService.java
+++ vendas/src/main/java/com/api/vendas/Vendas/VendasService.java
@@ -2,6 +2,9 @@ package com.api.vendas.Vendas;
 
 import com.api.vendas.Vendas.exception.ImovelNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,10 +13,15 @@ import java.util.List;
 
 @Service
 public class VendasService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Autowired
     private VendasRepository vendasRepository;
 
     public List<Venda> getVendas() {
+
         return vendasRepository.findAll();
     }
 
@@ -27,41 +35,56 @@ public class VendasService {
     }
 
     public List<Venda> getVendasByStatus(String status) {
+
+
+
         return vendasRepository.findByVendaStatus(status);
     }
 
 
-    public Venda postVenda(VendaCreateDTO vendaCreateDTO){
+    public Venda postVenda(VendaCreateDTO vendaCreateDTO, String token){
 
         Venda venda = new Venda();
 
-       RestTemplate restTemplate = new RestTemplate();
 
         venda.setVendaStatus("SUCESSO");
 
-        ResponseEntity<ImoveisDTO> responseImoveis =
-                restTemplate.getForEntity("http://localhost:8081/imoveis/" + vendaCreateDTO.getIdenifierImovel(), ImoveisDTO.class);
-        if (!responseImoveis.getStatusCode().is2xxSuccessful()) {
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add("token", token);
+
+
+        HttpEntity httpEntity = new HttpEntity<>(headers);
+
+        try{
+            ResponseEntity<ImoveisDTO> responseImoveis =  restTemplate.exchange("http://3.134.139.76:8080/imovel/" + vendaCreateDTO.getIdenifierImovel(),HttpMethod.GET,httpEntity, ImoveisDTO.class);
+            venda.setImovelIdentifier(responseImoveis.getBody().getIdentifier());
+        }
+        catch (RuntimeException ex){
             venda.setVendaStatus("ERRO");
-            throw new ImovelNotFoundException();
+            venda.setImovelIdentifier(vendaCreateDTO.getIdenifierImovel());
         }
 
-        venda.setImovelIdentifier(responseImoveis.getBody().getIdentifier());
-
-        ResponseEntity<CorretorDTO> responseCorretor =
-                restTemplate.getForEntity("http://localhost:8081/corretor/cpf/" + vendaCreateDTO.getCpfCorretor(), CorretorDTO.class);
-        if (!responseCorretor.getStatusCode().is2xxSuccessful()) {
+        try{
+            ResponseEntity<CorretorDTO> responseCorretor = restTemplate.exchange("http://35.87.155.27:8080/corretor/cpf/" + vendaCreateDTO.getCpfCorretor(),HttpMethod.GET, httpEntity, CorretorDTO.class);
+            venda.setCpfCorretor(responseCorretor.getBody().getCpf());
+        }
+        catch (RuntimeException ex){
             venda.setVendaStatus("ERRO");
+            venda.setCpfCorretor(vendaCreateDTO.getCpfCorretor());
         }
 
-        venda.setCpfCorretor(responseCorretor.getBody().getCpf());
 
-        ResponseEntity<ClienteDTO> responseCliente =
-                restTemplate.getForEntity("http://localhost:8081/cliente/" + vendaCreateDTO.getCpfCliente(), ClienteDTO.class);
-        if(! responseCliente.getStatusCode().is2xxSuccessful()){
-            venda.setVendaStatus("ERRO");
+        try{
+            ResponseEntity<ClienteDTO> responseCliente =
+                    restTemplate.exchange("http://34.210.87.17:8080/cliente/exists/" + vendaCreateDTO.getCpfCliente(),HttpMethod.GET, httpEntity, ClienteDTO.class);
+            venda.setCpfCliente(vendaCreateDTO.getCpfCliente());
+
         }
-        venda.setCpfCliente(responseCliente.getBody().getCpf());
+        catch (RuntimeException ex){
+            venda.setVendaStatus("ERRO");
+            venda.setCpfCliente(vendaCreateDTO.getCpfCliente());
+        }
 
 
         return vendasRepository.save(venda);
